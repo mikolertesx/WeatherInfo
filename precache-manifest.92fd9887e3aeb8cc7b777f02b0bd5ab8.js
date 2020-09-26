4f5de4cb055c8c137cf7be9b52b9ab214d412e14
Miguel Angel <mgangsal@outlook.com>
2020-09-26 17:35:15 -0500

    Updates

---
+++ precache-manifest.92fd9887e3aeb8cc7b777f02b0bd5ab8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1bf3391838b76ab6ed0e272ad24a3c7",
+    "url": "/WeatherInfo/index.html"
+  },
+  {
+    "revision": "c3f3fdbb62389f09037a",
+    "url": "/WeatherInfo/static/css/main.023d1b72.chunk.css"
+  },
+  {
+    "revision": "0fe8bd920f0fcbc55185",
+    "url": "/WeatherInfo/static/js/2.5becd2ff.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/WeatherInfo/static/js/2.5becd2ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3f3fdbb62389f09037a",
+    "url": "/WeatherInfo/static/js/main.a659948b.chunk.js"
+  },
+  {
+    "revision": "f71b314720679a7f4153",
+    "url": "/WeatherInfo/static/js/runtime-main.437fdc9e.js"
+  }
+]);
